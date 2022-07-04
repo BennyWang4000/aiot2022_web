296507fefa8ad1ada6780301ffab01bb51a82050
BennyWang4000 <wang.benny0102@gmail.com>
2022-07-04 08:46:54 +0000

    220704 connect sql

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 '''iptables
 iptables -I INPUT -j ACCEPT
 '''
-
 import sys
 import time
 import threading
 import random
 from flask import Flask, Blueprint, request, abort, render_template, jsonify, flash
 from flask_socketio import SocketIO, emit
-
 import json
+
+import datetime
+import pymssql
+
 from views.status.status import app_status
 from views.info.info import app_info
+
 
 app = Flask(__name__)
 app.register_blueprint(app_status)
@@ -21,9 +24,21 @@ blueprint = Blueprint('blueprint', __name__)
 app.config['SECRET_KEY'] = 'nutcadmin5566'
 socketio = SocketIO(app)
 
+
+print('connecting...')
+conn = pymssql.connect(
+    host='localhost',
+		# localhost:3306
+    user='root',
+    password='050610AIoT',
+    database='test_database',
+    charset='big5'
+)
+cursor = conn.cursor(as_dict=True)
+print('connected!')
+
 temp = 0.0
 humi = 0.0
-
 
 @socketio.on('temp')
 def emit_temp(temp, humi):
@@ -47,15 +62,21 @@ def emit_count(count):
 def setTempHumi():
     request_data = request.get_json()
     if request_data['password'] == 'nutcadmin5566':
+        count = 80 + random.randint(-10, 10)
         temp = request_data['temp']
         humi = request_data['humi']
         emit_temp(temp, humi)
+        now= datetime.datetime.now()
+
+        cusor.execute(f'insert into test_table(set_time, people_flow, temp, humidity, air_quality) values ({now}, {count}, {temp}, {humi}, -1)')
+
         # emit_humi(humi)
-        flash('correct password', 'info')
         return jsonify({'code': 200})
     else:
-        flash('wrong password', 'warning')
         return jsonify({'code': 400})
+
+
+
 
 
 @app.route("/")
@@ -64,18 +85,17 @@ def home():
     # return render_template('static/index.html', async_mode=socketio.async_mode)
 
 
-def counting():
-    while True:
-        count = 80 + random.randint(-10, 10)
-        time.sleep(5)
-        print(count, file=sys.stderr)
-        emit_count(count)
-        # emit_humi(humi)
-        emit_temp(30, 50)
+# def counting():
+#     while True:
+#         time.sleep(5)
+#         print(count, file=sys.stderr)
+#         emit_count(count)
+#         # emit_humi(humi)
+#         # emit_temp(30, 50)
 
 
-count_thread = threading.Thread(target=counting)
-count_thread.start()
+# count_thread = threading.Thread(target=counting)
+# count_thread.start()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5566")

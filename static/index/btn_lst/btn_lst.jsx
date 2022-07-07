e9704db653da75431f4fd90f43708d452f98bbc8
owner.laptop <wang.benny0102@gmail.com>
2022-07-07 13:23:34 +0800

    220707 run build pm

--- static/index/btn_lst/btn_lst.jsx
+++ static/index/btn_lst/btn_lst.jsx
@@ -8,7 +8,7 @@ function Btn_lst() {
             <ul className='btn_lst'>
                 <li style={{ display: "inline" }}><Btn idName="btn_temp" ioType="temp"></Btn></li>
                 <li style={{ display: "inline" }}><Btn idName="btn_humi" ioType="humi"></Btn></li>
-                <li style={{ display: "inline" }}><Btn idName="btn_humi" ioType="pm"></Btn></li>
+                <li style={{ display: "inline" }}><Btn idName="btn_pm" ioType="pm"></Btn></li>
             </ul>
         </div>
     )

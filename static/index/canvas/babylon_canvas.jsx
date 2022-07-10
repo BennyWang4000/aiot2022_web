94a82e63491bde4482b80332aa7b42f549e7d58a
owner.laptop <wang.benny0102@gmail.com>
2022-07-10 16:44:55 +0800

    220710 resume nav tag

--- static/index/canvas/babylon_canvas.jsx
+++ static/index/canvas/babylon_canvas.jsx
@@ -177,9 +177,6 @@ function Baylon_canvas() {
                         btn_round.color = "white";
                         btn_round.fontSize = 70;
                         btn_round.background = "green";
-                        btn_round.onPointerUpObservable.add(function () {
-                            alert("you did it!");
-                        });
                         texture_round.addControl(btn_round);
                     
                         // var plane_loading = BABYLON.Mesh.CreatePlane("plane_loading", 300, 0, scene);
